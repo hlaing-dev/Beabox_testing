bd1091a83613b91c57b381ca610a9cfd0238e286
hlaing-dev <hlaingmin1235145@gmail.com>
2025-02-06 16:45:33 +0700

    update

--- src/page/wallet/comp/Invite.tsx
+++ src/page/wallet/comp/Invite.tsx
@@ -1,56 +1,100 @@
 import React, { useRef, useState } from "react";
 import Header from "../Header";
-import { useGetInviteQuery } from "@/store/api/wallet/walletApi";
 import qr from "../../../assets/wallet/qr.svg";
 import "../wallet.css";
 import { Link } from "lucide-react";
 import { useGetMyProfileQuery } from "@/store/api/profileApi";
 import { toPng } from "html-to-image";
 import shareLogo from "../../../assets/share-logo.svg";
+import { useGetConfigQuery } from "@/page/home/services/homeApi";
+import { showToast } from "@/page/home/services/errorSlice";
+import { useDispatch } from "react-redux";
+import { QRCodeCanvas } from "qrcode.react";
 interface InviteProps {}
 
 const Invite: React.FC<InviteProps> = ({}) => {
-  const { data } = useGetInviteQuery("");
+  const { data } = useGetConfigQuery("");
   const { data: ppdaata } = useGetMyProfileQuery("");
   const [copied, setCopied] = useState<boolean>(false);
   const imageRef = useRef<HTMLDivElement>(null);
-
-  console.log(data);
-
-  const sendEventToNative = (name: string, text: string) => {
-    if (
-      (window as any).webkit &&
-      (window as any).webkit.messageHandlers &&
-      (window as any).webkit.messageHandlers.jsBridge
-    ) {
-      (window as any).webkit.messageHandlers.jsBridge.postMessage({
-        eventName: name,
-        value: text,
-      });
-    }
-  };
-
-  console.log(data?.data?.share_link);
-
-  const handleCopyCode = () => {
-    sendEventToNative("copyShareUrl", `${ppdaata?.data?.user_code}`);
-  };
-
-  // const handleCopyCode = () => {
-  //   if (ppdaata?.data) {
-  //     const inviteCode = ppdaata.data.user_code;
-  //     navigator.clipboard.writeText(inviteCode).then(() => {
-  //       setCopied(true); // Set copied to true immediately
-  //       setTimeout(() => {
-  //         setCopied(false); // Reset copied to false after 2 seconds
-  //       }, 2000);
-  //     });
-  //   }
-  // };
-
-  const handleSaveAsImage = () => {
-    sendEventToNative("saveImage", `${data?.data?.pop_up?.image}`);
-  };
+  const dispatch = useDispatch();
+  
+    const shareUrl = data?.share_link;
+    const appDownloadLink = data?.app_download_link;
+  
+    const isIOSApp = () => {
+      return (
+        (window as any).webkit &&
+        (window as any).webkit.messageHandlers &&
+        (window as any).webkit.messageHandlers.jsBridge
+      );
+    };
+  
+    const sendEventToNative = (name: string, text: string) => {
+      if (
+        (window as any).webkit &&
+        (window as any).webkit.messageHandlers &&
+        (window as any).webkit.messageHandlers.jsBridge
+      ) {
+        (window as any).webkit.messageHandlers.jsBridge.postMessage({
+          eventName: name,
+          value: text,
+        });
+      }
+    };
+  
+    const handleShare = () => {
+      if (isIOSApp()) {
+        sendEventToNative("saveImage", shareUrl);
+      } else {
+        const qrCanvas = document.querySelector("canvas"); // Select the canvas element
+  
+        if (qrCanvas) {
+          const imageType = "image/png"; // Change to "image/jpeg" for JPG
+          const imageUrl = qrCanvas.toDataURL(imageType); // Convert canvas to data URL
+  
+          // Create a temporary link to trigger the download
+          const link = document.createElement("a");
+          link.href = imageUrl;
+          link.download = "QRCode.png"; // Set file name and extension
+          document.body.appendChild(link); // Append link to the body
+          link.click(); // Trigger download
+          document.body.removeChild(link); // Remove link after download
+        } else {
+          console.error("QR code canvas not found.");
+        }
+      }
+    };
+  
+    const handleAppCopy = () => {
+      if (isIOSApp()) {
+        sendEventToNative("copyAppdownloadUrl", appDownloadLink);
+      } else {
+        navigator.clipboard.writeText(appDownloadLink).then(() => {
+          dispatch(
+            showToast({
+              message: "复制成功",
+              type: "success",
+            })
+          );
+        });
+      }
+    };
+  
+    const handleLinkCopy = () => {
+      if (isIOSApp()) {
+        sendEventToNative("copyShareUrl", shareUrl);
+      } else {
+        navigator.clipboard.writeText(shareUrl).then(() => {
+          dispatch(
+            showToast({
+              message: "复制成功",
+              type: "success",
+            })
+          );
+        });
+      }
+    };
 
   // const handleSaveAsImage = () => {
   //   if (imageRef.current) {
@@ -86,12 +130,21 @@ const Invite: React.FC<InviteProps> = ({}) => {
               className=" mt-[60px] p-10 rounded-lg flex flex-col gap-[30px] justify-center items-center img-bg"
             >
               <img src={shareLogo} className="h-[40px]" />
-              <img className=" w-[180px] h-[180px]" src={qr} alt="" />
+              {/* <img className=" w-[180px] h-[180px]" src={qr} alt="" /> */}
+              <div className="flex justify-center items-center p-0">
+            <div className="text-right bg-white p-3 rounded-xl">
+              <QRCodeCanvas
+                value={shareUrl}
+                size={180}
+                className="qr_download"
+              />
+            </div>
+          </div>
               <p className=" text-[#888] text-[12px] font-[400] leading-[14px] text-center">
                 分享此邀请码邀请您的朋友下载app，即可领取 Bebit 币！
               </p>
               <button
-                onClick={handleCopyCode}
+                onClick={handleLinkCopy}
                 className=" flex justify-center items-center gap-[6px] px-[20px] py-[12px] copy_btn"
               >
                 <Link className=" w-[14px] h-[14px]" />
@@ -110,17 +163,16 @@ const Invite: React.FC<InviteProps> = ({}) => {
               </div>
             </div>
           )}
-
           {/* two buttons */}
           <div className=" flex justify-center items-center gap-[24px]">
             <button
-              onClick={handleSaveAsImage}
+              onClick={handleShare}
               className="px-[20px] py-[12px] copy_btn text-white text-[12px] font-[400] leading-[20px]"
             >
               保存邀请码
             </button>
             <button
-              onClick={handleCopyCode}
+              onClick={handleAppCopy}
               className="px-[20px] py-[12px] copy_btn text-white text-[12px] font-[400] leading-[20px]"
             >
               复制邀请链接
