b897e6cee9b14c1939be30abf59a5d7691fabb40
hlaing-dev <hlaingmin1235145@gmail.com>
2025-03-17 18:33:01 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Landing from "./components/Landing";
 import Routing from "./routes/Routing";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,6 +14,50 @@ const App = () => {
   const { panding } = useSelector((state: any) => state.model);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data } = useGetApplicationAdsQuery("");
+
+  const [isMobileBrowser, setIsMobileBrowser] = useState(false);
+
+  useEffect(() => {
+    // Function to check if the user is on a mobile browser
+    const checkIfMobileBrowser = () => {
+      const userAgent = navigator.userAgent;
+      const isAndroid = /Android/i.test(userAgent); // Check only for Android
+
+      setIsMobileBrowser(isAndroid);
+    };
+
+    // Function to adjust the height of the video element
+    const adjustVideoHeight = () => {
+      const videoElement = document.querySelector(".video");
+      const videoElement1 = document.querySelector(".video1");
+      if (videoElement) {
+        if (isMobileBrowser) {
+          // Adjusted height for mobile browsers
+          videoElement.style.height = "calc(100dvh - 70px)";
+        }
+      }
+      if (videoElement1) {
+        if (isMobileBrowser) {
+          // Adjusted height for mobile browsers
+          videoElement1.style.height = "calc(100dvh - 0px)";
+        }
+      }
+    };
+
+    // Check if the user is on a mobile browser
+    checkIfMobileBrowser();
+
+    // Adjust the height initially
+    adjustVideoHeight();
+
+    // Add a resize event listener to handle viewport changes
+    window.addEventListener("resize", adjustVideoHeight);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", adjustVideoHeight);
+    };
+  }, [isMobileBrowser]);
 
   const dispatch = useDispatch();
 
